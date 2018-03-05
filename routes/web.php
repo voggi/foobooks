b7aebf4dafd1f708f47fa7d9373705cf1688c2e4
Nicolas Vogelpoth <nicolas.vogelpoth@gmail.com>
2018-03-05 23:20:47 +0100

    Working along ...

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/example', function () {
-    return view('abc');
+Route::get('/books', function () {
+    return 'Here are all the books in the library';
+});
+
+Route::get('/books/{title}', function ($title) {
+    return 'You are viewing the book ' . $title;
 });
