74deee325d038c36f055c0bf4aa7c193acfcba44
Nicolas Vogelpoth <nicolas.vogelpoth@gmail.com>
2018-04-03 21:28:50 +0200

    More error testing

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,7 +14,7 @@ class PageController extends Controller
 
     public function about()
     {
-        return view('pages.aboutt');
+        return view('pages.about');
     }
 
     public function contact()
